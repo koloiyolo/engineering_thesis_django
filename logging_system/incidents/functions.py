dae5b3e26c824ac5eae7ba802c5db454b9f66986
Jakub Kołodziej <jakvb.kolodziej@gmail.com>
2024-10-26 17:45:09 +0200

    Final fixes in incidents

--- logging_system/incidents/functions.py
+++ logging_system/incidents/functions.py
@@ -22,8 +22,8 @@ def create_incident(system=None, log=None):
             system=system,
             tag=1,
             title= f" Possible anomaly found in {system.name}'s logs",
-            message=f'At {incident.date}{incident.time} following log record was detected on {inciudent.system.name} {inciudent.system.ip}: \n' +
-                    'Tags: ' + log.program + ' Message: ' + log.message + '\n',
+            message=f'At {log.datetime} Following log record was detected on {system.name} {system.ip}: \n' +
+                    f'Tags: {log.program} Message: {log.message} \n',
             ip=system.ip)
         if system.email_notify:
             email = create_email(incident=incident, log=log)
@@ -37,7 +37,7 @@ def create_incident(system=None, log=None):
             system=system,
             tag=0,
             title= f" System {system.name} {system.ip} is down.",
-            message=f'System ip: {incident.ip} \nMessage: {incident.message} \nDate: {incident.date}{incident.time}',
+            message=f'System ip: {system.ip} \nMessage: System {system.name} is down \nDate: {datetime.now()}',
             ip=system.ip)
         if system.email_notify:
             email = create_email(incident=incident)
@@ -59,15 +59,13 @@ def create_email(incident=None, log=None):
     if notifications_mode != 0: 
         if incident.tag == 0 and (notifications_mode == 1 or notifications_mode == 3):
             return (
-                    f'System {incident.system.name} is down',
+                    incident.title,
                     incident.message,
-                    'from@example.com',
                     send_to)
         elif incident.tag == 1 and (notifications_mode == 2 or notifications_mode == 3):
             return (
-                    f"Abnormal record detected in {incident.system.name}'s logs",
+                    incident.title,
                     incident.message,
-                    'from@example.com',
                     send_to)
         else:
             print("Email disabled for this type of incidents")
