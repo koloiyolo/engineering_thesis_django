067a0695836bf826520415b0000b98e7e9b5bd65
Jakub Kołodziej <jakvb.kolodziej@gmail.com>
2024-12-14 20:23:07 +0100

    change datetime field from DateField to DateTimeField in AuditLog class

--- logging_system/audit_log/models.py
+++ logging_system/audit_log/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class AuditLog(models.Model):
-    datetime = models.DateField(auto_now=True)
+    datetime = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     text = models.CharField(max_length=200)
