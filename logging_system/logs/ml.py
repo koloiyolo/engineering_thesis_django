1d9c3f5f9646eceb77e80a6f729982a01428172f
Jakub Kołodziej <jakvb.kolodziej@gmail.com>
2024-12-21 17:14:59 +0100

    first commit

--- logging_system/logs/ml.py
+++ logging_system/logs/ml.py
@@ -22,12 +22,12 @@ from incidents.functions import create_incident
 # from .functions import get_logs, send_anomaly_emails
 
 # get logs from database
-def get_logs(get_count):
+def get_logs(count):
     data = None
-    count = Log.objects.all().count()
-    if count > get_count:
-        offset = random.randint(0, (count - get_count))
-        data = Log.objects.all()[offset:offset + get_count]
+    _count = Log.objects.all().count()
+    if count > _count:
+        offset = random.randint(0, (_count - count))
+        data = Log.objects.all()[offset:offset + count]
     else:
         data = Log.objects.all()
     
@@ -38,6 +38,27 @@ def get_logs(get_count):
         return data
 
     return data
+
+# zips logs with labels and saves them to database
+# creates incidents
+# returns emails
+def zip_logs(logs=None, labels=None, anomaly_label=0):
+
+    if logs is None:
+        return None, "Logs dont exist"
+
+    if labels is None:
+        return None, "Labels dont exist"
+
+    emails = []
+    for log, label in zip(logs, labels):
+            log.label = label
+            log.save()
+            if log.label == anomaly_label:
+                email = create_incident(log=log)
+                if email is not False:
+                    emails.append(email)
+    return emails, "Zipping complete"
 
 
 # sklearn ML train function prototype
@@ -63,46 +84,40 @@ def train(model=KMeans(2, random_state=42), file_prefix='kmeans'):
 
     return False
 
-# sklearn ML classify function prototype
+
+# sklearn ML classify function
 def classify(file_prefix = 'kmeans'):
-    emails = []
+    emails = None
     clf_file = file_prefix + '.joblib'
     encoder_file = file_prefix + '_encoder.joblib'
-    if os.path.exists(clf_file) and os.path.exists(encoder_file):
-        data = get_logs(Settings.load().ml_classify)
-        if data is None:
-            print("No data to classify")
-            return True
 
-        features = [obj.get_features() for obj in data]
-
-        encoder = joblib.load(encoder_file)
-        encoded_features = encoder.transform(features).toarray()
-        X = np.array(encoded_features)
-
-        clf = joblib.load(clf_file)
-        clf.fit(X)
-        labels = clf.labels_
-        anomaly = Settings.load().ml_anomaly_cluster
-        for log, label in zip(data, labels):
-            log.label = label
-            log.save()
-            if log.label == anomaly:
-                email = create_incident(log=log)
-                if email is not False:
-                    emails.append(email)
-
-
-        print(emails)
-        if len(emails) != 0:
-            send_mass_mail(emails)
-
-        # send_anomaly_emails(data, debug=True)
-
-        return True
-    else:
+    if (os.path.exists(clf_file) and os.path.exists(encoder_file)) is False:
         return False
-    return False
+
+    data = get_logs(Settings.load().ml_classify)
+    if data is None:
+        print("No data to classify")
+        return True
+
+    features = [obj.get_features() for obj in data]
+    encoder = joblib.load(encoder_file)
+    encoded_features = encoder.transform(features).toarray()
+    X = np.array(encoded_features)
+    clf = joblib.load(clf_file)
+    labels = clf.predict(X)
+    
+    emails, message = zip_logs(
+        logs=data, 
+        labels=labels, 
+        anomaly_label = Settings.load().ml_anomaly_cluster
+        )
+
+    print(emails)
+    if emails is not None and len(emails) != 0:
+        send_mass_mail(emails)
+    # send_anomaly_emails(data, debug=True)
+
+    return True
 
 #######################################################################################################
 
@@ -136,27 +151,22 @@ def classify_ahc():
 def train_som():
     data = get_logs(Settings.load().ml_train)
     clusters = Settings.load().ml_clusters
-    if data is not None:
-        file_prefix = 'som'
-        features = [obj.get_features() for obj in data]
-        encoder = TfidfVectorizer()
-        encoded_features = encoder.fit_transform(features).toarray()
-        input_len = encoded_features.shape[1]
-
-        som = MiniSom(x=20, y=clusters, input_len=input_len, sigma=1, learning_rate=0.5)
-        X = np.array(encoded_features)
-        som.random_weights_init(encoded_features)
-        som.train_random(encoded_features, 1000)
-
-        joblib.dump(som, 'som.joblib')
-        joblib.dump(encoder, 'som_encoder.joblib')
-
+    if data is None:
         return True
 
-    else:
-        return False
-
-    return False
+    file_prefix = 'som'
+    features = [obj.get_features() for obj in data]
+    encoder = TfidfVectorizer()
+    encoded_features = encoder.fit_transform(features).toarray()
+    input_len = encoded_features.shape[1]
+    som = MiniSom(x=20, y=clusters, input_len=input_len, sigma=1, learning_rate=0.5)
+    X = np.array(encoded_features)
+    som.random_weights_init(encoded_features)
+    som.train_random(encoded_features, 1000)
+    joblib.dump(som, 'som.joblib')
+    joblib.dump(encoder, 'som_encoder.joblib')
+    
+    return True
 
 def classify_som():
     emails = []
